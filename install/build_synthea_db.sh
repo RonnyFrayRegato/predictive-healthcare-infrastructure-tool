f001b7c81de68e09b987673893c4c4afb2ee2df2
Ronny Fray <ronnyfray@gmail.com>
2023-04-13 22:16:05 -0400

    Update build_synthea_db.sh

--- install/build_synthea_db.sh
+++ install/build_synthea_db.sh
@@ -6,20 +6,20 @@ sudo docker start pg
 sudo docker start synthea
 
 # Delete existing database
-sudo docker exec -it pg psql -U postgres -c "DROP DATABASE phit WITH (FORCE);"
+sudo docker exec -i pg psql -U postgres -c "DROP DATABASE phit WITH (FORCE);"
 
 # Create PHIT database and Synthea schema
 echo "Creating Synthea database..."
-sudo docker exec -it pg psql -U postgres -c "CREATE DATABASE phit;"
-sudo docker exec -it pg psql -U postgres -d phit -c "CREATE SCHEMA IF NOT EXISTS synthea;"
+sudo docker exec -i pg psql -U postgres -c "CREATE DATABASE phit;"
+sudo docker exec -i pg psql -U postgres -d phit -c "CREATE SCHEMA IF NOT EXISTS synthea;"
 
 # Create relation structure for database
 echo "Creating relation structure for database..."
 sudo docker cp /home/ubuntu/predictive-healthcare-infrastructure-tool/install/sql/synthea_template.sql pg:/home
-sudo docker exec -it pg psql -U postgres -d phit -f "/home/synthea_template.sql"
+sudo docker exec -i pg psql -U postgres -d phit -f "/home/synthea_template.sql"
 
 # Generate Synthea Data
-sudo docker exec -it synthea bash -c "java -jar synthea-with-dependencies.jar -p 1000 --exporter.csv.export true"
+sudo docker exec -i synthea bash -c "java -jar synthea-with-dependencies.jar -p 1000 --exporter.csv.export true"
 
 # Copy CSV contents into database
 echo "Copying CSV contents into database..."
@@ -27,10 +27,10 @@ declare -a array=("patients" "organizations" "providers" "payers" "encounters" "
   "conditions" "devices" "imaging_studies" "immunizations" "medications" "observations" "procedures" "supplies")
 
 for FILE in "${array[@]}"; do
-  sudo docker exec -it pg psql -U postgres -d phit -c "SET schema 'synthea';" -c "COPY ${FILE} FROM '/home/${FILE}.csv' WITH (format csv, header true, delimiter ',');"
+  sudo docker exec -i pg psql -U postgres -d phit -c "SET schema 'synthea';" -c "COPY ${FILE} FROM '/home/${FILE}.csv' WITH (format csv, header true, delimiter ',');"
 done
 
 # Create database Views
 echo "Creating database views..."
 sudo docker cp /home/ubuntu/predictive-healthcare-infrastructure-tool/install/sql/views_template.sql pg:/home
-sudo docker exec -it pg psql -U postgres -d phit -f "/home/views_template.sql"
+sudo docker exec -i pg psql -U postgres -d phit -f "/home/views_template.sql"
